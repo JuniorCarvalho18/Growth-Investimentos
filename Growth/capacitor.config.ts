218eee071543433d5ea6988038fc7bf81341723d
RyuSenshi <laerciojrcarvalho18@outlook.com>
2025-05-01 22:31:53 -0400

    Muita coisa

--- Growth/capacitor.config.ts
+++ Growth/capacitor.config.ts
@@ -1,9 +1,27 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.Growth',
   appName: 'Growth',
-  webDir: 'www'
+  webDir: 'www',
+  "plugins": {
+    "SplashScreen": {
+      "launchShowDuration": 0,
+      "launchAutoHide": true,
+      "backgroundColor": "green",
+      "androidSplashResourceName": "splash",
+      "androidScaleType": "CENTER_CROP",
+      "showSpinner": false,
+      "androidSpinnerStyle": "large",
+      "iosSpinnerStyle": "small",
+      "spinnerColor": "#999999",
+      "splashFullScreen": true,
+      "splashImmersive": true,
+      "layoutName": "launch_screen",
+      "useDialog": false,
+    },
+  },
+
 };
 
 export default config;
--- Growth/src/app/app.component.ts
+++ Growth/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SplashScreen } from '@capacitor/splash-screen';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -7,13 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Home', url: '/folder/home', icon: 'home' },
+    { title: 'Marketplace', url: '/folder/marketplace', icon: 'bag' },
+    { title: 'Configurações', url: '/folder/configuracoes', icon: 'settings' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  constructor() {
+    this.ShowSplash();
+  }
+
+ async ShowSplash() {
+      await SplashScreen.show({
+        autoHide: true,
+        showDuration: 10000,
+      });
+
+}
 }
